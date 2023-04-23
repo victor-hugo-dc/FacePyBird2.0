f7ce68699b652e2d908fcd59cecb51f2e83d1896
Victor Del Carpio <vhdelcg@gmail.com>
2023-04-23 18:23:26 -0400

    bird component

---
+++ bird.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+from constants import *
+
+class Bird:
+    def __init__(self) -> None:
+        self.players: list = [
+            cv2.imread('assets/sprites/bluebird-upflap.png', cv2.IMREAD_UNCHANGED),
+            cv2.imread('assets/sprites/bluebird-midflap.png', cv2.IMREAD_UNCHANGED),
+            cv2.imread('assets/sprites/bluebird-downflap.png', cv2.IMREAD_UNCHANGED)
+        ]
+
+        self.current_player: int = 0
+        self.player: np.ndarray = self.players[self.current_player]
+        self.speed: int = SPEED
+
+        self.x: int = SCREEN_WIDTH // 6
+        self.y: int = SCREEN_HEIGHT // 3
+
+        self.width = self.player.shape[1]
+    
+    def update(self) -> None:
+        self.current_image = (self.current_player + 1) % 3
+        self.image = self.players[self.current_player]
+        self.speed += GRAVITY
+
+        self.y += self.speed
+    
+    def bump(self):
+        self.speed = -SPEED
+    
+    def begin(self):
+        self.current_player = (self.current_player + 1) % 3
+        self.player = self.players[self.current_image]
