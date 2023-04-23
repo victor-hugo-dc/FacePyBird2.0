0f66ee3a4814fb11475bf84b7f8e574f4b48c808
Victor Del Carpio <vhdelcg@gmail.com>
2023-04-23 18:24:06 -0400

    ground/base component

---
+++ ground.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+from constants import *
+
+class Ground:
+    def __init__(self, xpos: int) -> None:
+        self.base = cv2.imread('assets/sprites/base.png', -1)
+        self.base = cv2.cvtColor(self.base, cv2.COLOR_RGB2RGBA)
+        self.base = cv2.resize(self.base, (GROUND_WIDTH, GROUND_HEIGHT))
+
+        self.x = xpos
+        self.y = SCREEN_HEIGHT - GROUND_HEIGHT
+
+        self.width = GROUND_WIDTH
+    
+    def update(self):
+        self.x -= GAME_SPEED
