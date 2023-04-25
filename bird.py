0f2ed9eb8845778c3ea968aebf8c6b756bda67e4
Victor Del Carpio <vhdelcg@gmail.com>
2023-04-24 23:39:54 -0400

    random selection of birds

--- bird.py
+++ bird.py
@@ -4,11 +4,25 @@ from constants import *
 
 class Bird:
     def __init__(self) -> None:
-        self.players: list = [
+        self.red_bird: list = [
+            cv2.imread('assets/sprites/redbird-upflap.png', cv2.IMREAD_UNCHANGED),
+            cv2.imread('assets/sprites/redbird-midflap.png', cv2.IMREAD_UNCHANGED),
+            cv2.imread('assets/sprites/redbird-downflap.png', cv2.IMREAD_UNCHANGED)
+        ]
+
+        self.blue_bird: list = [
             cv2.imread('assets/sprites/bluebird-upflap.png', cv2.IMREAD_UNCHANGED),
             cv2.imread('assets/sprites/bluebird-midflap.png', cv2.IMREAD_UNCHANGED),
             cv2.imread('assets/sprites/bluebird-downflap.png', cv2.IMREAD_UNCHANGED)
         ]
+
+        self.yellow_bird: list = [
+            cv2.imread('assets/sprites/yellowbird-upflap.png', cv2.IMREAD_UNCHANGED),
+            cv2.imread('assets/sprites/yellowbird-midflap.png', cv2.IMREAD_UNCHANGED),
+            cv2.imread('assets/sprites/yellowbird-downflap.png', cv2.IMREAD_UNCHANGED)
+        ]  
+
+        self.players = random.choice([self.red_bird, self.blue_bird, self.yellow_bird])
 
         self.current_player: int = 0
         self.player: np.ndarray = self.players[self.current_player]
