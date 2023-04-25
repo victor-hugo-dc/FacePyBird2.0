8d6d3e75edade8609b8fbc393f2fe8401bea5d6a
Victor Del Carpio <vhdelcg@gmail.com>
2023-04-25 12:38:34 -0400

    change size of message

--- message.py
+++ message.py
@@ -3,6 +3,7 @@ import cv2
 class Message:
     def __init__(self) -> None:
         self.image = cv2.imread('assets/sprites/message.png', -1)
+        self.image = cv2.resize(self.image, (135, 180), interpolation = cv2.INTER_AREA)
         self.height, self.width, _ = self.image.shape
     
     def message(self, x: int, y: int):
