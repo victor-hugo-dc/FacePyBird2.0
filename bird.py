57851815beb11174e1b266fa1d6b45d2713796b0
Victor Del Carpio <vhdelcg@gmail.com>
2023-04-25 13:11:24 -0400

    bird now rotates as it flaps

--- bird.py
+++ bird.py
@@ -33,22 +33,34 @@ class Bird:
         self.y: int = SCREEN_HEIGHT // 3
 
         self.height, self.width, _ = self.player.shape
+        self.center: tuple = tuple(np.array((self.width, self.height)) / 2)
+
+        self.rotation: int = 45
+        self.visible_rotation: int = 20
     
     def update(self) -> None:
         self.current_player = (self.current_player + 1) % 3
         self.player = self.players[self.current_player]
 
         self.y += self.speed
-    
+
+        self.update_rotation()
+
+        rotation_matrix: np.ndarray = cv2.getRotationMatrix2D(self.center, self.visible_rotation, 1.0)
+        self.player = cv2.warpAffine(self.player, rotation_matrix, (self.width, self.height), flags = cv2.INTER_LINEAR)
+
     def update_speed(self) -> None:
         if self.speed < VELOCITY_MAX:
             self.speed += GRAVITY
+    
+    def update_rotation(self) -> None:
+        if self.rotation > -90:
+            self.rotation -= ROTATION_VELOCITY
+        
+        self.visible_rotation = self.rotation if self.rotation <= ROTATION_THRESHOLD else ROTATION_THRESHOLD
     
     def bump(self, acceleration) -> None:
         if acceleration:
             playsound.playsound(WING, False)
             self.speed = acceleration
-    
-    def begin(self):
-        self.current_player = (self.current_player + 1) % 3
-        self.player = self.players[self.current_player]
+            self.rotation = 45
